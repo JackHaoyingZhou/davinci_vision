8a4cad8a6fa77ab05cb31fa39308f05531ee2467
nsujumnong <nsujumnong@wpi.edu>
2019-09-16 13:10:48 -0400

    delete old build directories

--- image_subscribe_test/main.cpp
+++ image_subscribe_test/main.cpp
@@ -1,40 +1,89 @@
 // Testing code for ROS to OpenCV image subscription
 
 #include <iostream>
-#include <stdio.h>
 
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
-#include <sensor_msgs/image_encodings.h>
-#include <cv_bridge/cv_bridge.h>
-
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/Image.h>
 
-using namespace std;
-using namespace cv;
+//void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+//{
+//    try
+//    {
+//        cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvShare(msg, "bgr8");
+//        cv::imwrite("rgb.bmp", cv_ptr->image);
+//        cv::imshow("view", cv_bridge::toCvShare(msg,"bgr8")->image);
+//        cv::waitKey(30);
+//    }
+//    catch(cv_bridge::Exception& e)
+//    {
+//        ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+//    }
+//}
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msg)
-{
-    try
-    {   
-        imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
-        waitKey(30);
-    }
-    catch(cv_bridge::Exception& e)
-    {
-        ROS_ERROR("Could not convert from '&s' to 'bgr8'.", msg->encoding.c_str());
+void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
+    try {
+        cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
+        cv::imwrite("rgb.bmp", cv_ptr->image);
+        cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
+        cv::waitKey(30);
+    } catch (cv_bridge::Exception& e) {
+        ROS_ERROR("Could not convert from '%s' to 'bgr8'.",
+                msg->encoding.c_str());
     }
 }
+
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "image_listener");
-    ros::NodeHandle nh;
-    cv::namedWindow("view");
-    cv::startWindowThread();
-    image_transport::ImageTransport it(nh);
-    image_transport::Subscriber sub = it.subscribe("ambf/image_data/default_camera", 1, imageCallback);
-    ros::spin();
-//    cv::destroyWindow("view");
+      ros::init(argc, argv, "image_listener");
+      ros::NodeHandle nh;
+      cv::namedWindow("view");
+      cv::startWindowThread();
+      image_transport::ImageTransport it(nh);
+      image_transport::Subscriber sub = it.subscribe("camera/image", 1, imageCallback);
+      ros::spin();
+      cv::destroyWindow("view");
+      return 0;
 }
+
+//#include <iostream>
+//#include <stdio.h>
+
+//#include <ros/ros.h>
+//#include <image_transport/image_transport.h>
+//#include <sensor_msgs/image_encodings.h>
+//#include <cv_bridge/cv_bridge.h>
+
+//#include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/imgproc/imgproc.hpp>
+
+//using namespace std;
+//using namespace cv;
+
+//void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+//{
+//    try
+//    {
+//        imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
+//        waitKey(30);
+//    }
+//    catch(cv_bridge::Exception& e)
+//    {
+//        ROS_ERROR("Could not convert from '&s' to 'bgr8'.", msg->encoding.c_str());
+//    }
+//}
+
+//int main(int argc, char **argv)
+//{
+//    ros::init(argc, argv, "image_listener");
+//    ros::NodeHandle nh;
+//    cv::namedWindow("view");
+//    cv::startWindowThread();
+//    image_transport::ImageTransport it(nh);
+//    image_transport::Subscriber sub = it.subscribe("camera/image", 1, imageCallback);
+//    ros::spin();
+////    cv::destroyWindow("view");
+//}
